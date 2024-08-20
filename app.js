67d71e54bbccff07e39b3a7524b6d0bf62f46410
mohm33443 <hassanelgohary@std.mans.edu.eg>
2024-08-20 17:10:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var name="mohamed hassan "
+var age = 25;
+var title="mansoura"
+var phone ="01142504846"
+console.log( name , age, title, phone)
+console.log(typeof age)
+console.log(typeof phone )
+
+let x = 30;
+function hello() {
+  x = 10;
+}
+console.log(x);
+
+
+let o=20
+function hello(){
+   o=10;
+ console.log(o)
+}
+hello();
+console.log(o);
